8759351f0e51dbbe5508ce4c70e1b890269a73ae
tKlancelot <tarik.louatah@gmail.com>
2025-06-23 01:11:45 +0200

    debug fetch jumbo.html

--- src/parts/jumbo.js
+++ src/parts/jumbo.js
@@ -1,7 +1,7 @@
 export default async function Jumbo() {
   const base = import.meta.env.BASE_URL || '/';
   try {
-    const response = await fetch(`${base}/public/parts/jumbo.html`);
+    const response = await fetch(`${base}parts/jumbo.html`);
     if (!response.ok) {
       console.error('Erreur de chargement de jumbo.html');
       return '<div>Erreur lors du chargement du composant Jumbo.</div>';
